2ab00b43e321f7799d34b06fce0221761f3a90f9
Roman <aventawonk@gmail.com>
2017-07-29 23:48:59 +0300

    Add backend folder

---
+++ backend/AppBundle/Controller/TaskController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Task;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskController extends Controller
+{
+    /**
+     * @Route("task/get", name="homepage")
+     */
+    public function getAction()
+    {
+        $tasks = $this->getDoctrine()
+          ->getRepository(Task::class)
+          ->findAll();
+
+        $callback = function($dateTime) {
+            return $dateTime->format(\DateTime::ISO8601);
+        };
+
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCallbacks(['date' => $callback]);
+
+        $serializer = new Serializer(
+          [$normalizer],
+          [new JsonEncoder()]
+        );
+
+        $jsonTasks = $serializer->serialize($tasks, 'json');
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->setContent($jsonTasks);
+
+        return $response;
+    }
+
+    /**
+     * @Route("task/add/{text}")
+     */
+    public function addAction($text)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $task = new Task();
+        $task->setText($text);
+        $task->setDate( new \DateTime());
+
+        $em->persist($task);
+        $em->flush();
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->setContent(1);
+        return $response;
+    }
+
+    /**
+     * @Route("task/update/{id}/{text}")
+     */
+    public function updateAction($id, $text)
+    {
+        $task = $this->getDoctrine()
+          ->getRepository(Task::class)
+          ->findOneById($id);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+          if ($task) {
+            $task->setText($text);
+
+            $em->persist($task);
+            $em->flush();
+
+
+            $response->setContent(1);
+            return $response;
+          }
+
+        $response->setContent('error');
+        return $response;
+    }
+
+    /**
+     * @Route("task/delete/{id}")
+     */
+    public function deleteAction($id)
+    {
+        $task = $this->getDoctrine()
+          ->getRepository(Task::class)
+          ->findOneById($id);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        if ($task) {
+          $em->remove($task);
+          $em->flush();
+
+          $response->setContent(1);
+          return $response;
+        }
+
+        $response->setContent('error');
+        return $response;
+    }
+}
---
+++ backend/AppBundle/Entity/Task.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Tosk
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Task
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+}
